6d079309a6e4be5fa0a2b840bc24f26c8b04a8ab
ppaunovski <paunovskipavel@gmail.com>
2024-04-26 22:36:07 +0200

    BookingNotFoundException added

--- Backend/src/main/kotlin/com/sorsix/backend/service/BookingService.kt
+++ Backend/src/main/kotlin/com/sorsix/backend/service/BookingService.kt
@@ -2,12 +2,13 @@ package com.sorsix.backend.service
 
 import com.sorsix.backend.domain.entities.Booking
 import com.sorsix.backend.repository.booking_repository.BookingRepository
+import com.sorsix.backend.service.exceptions.BookingNotFoundException
 import org.springframework.stereotype.Service
 
 @Service
 class BookingService(val bookingRepository: BookingRepository) {
     fun findAllBookings() = bookingRepository.findAll()
-    fun findBookingById(id: Long) = bookingRepository.findById(id)
+    fun findBookingById(id: Long) = bookingRepository.findById(id) ?: throw BookingNotFoundException(id)
     fun saveBooking(booking: Booking) = bookingRepository.save(booking)
     fun deleteBookingById(id: Long) = bookingRepository.deleteById(id)
 }
---
+++ Backend/src/main/kotlin/com/sorsix/backend/service/exceptions/BookingNotFoundException.kt
@@ -0,0 +1,4 @@
+package com.sorsix.backend.service.exceptions
+
+class BookingNotFoundException(id: Long?): RuntimeException("Booking with id $id not found!") {
+}
