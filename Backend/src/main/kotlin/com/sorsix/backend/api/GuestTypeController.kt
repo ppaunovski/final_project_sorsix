7abc61cce4fc815e6ad42afdf9008a2a91c86a8c
nikola.nacevski <nikola.nacevski@sorsix.com>
2024-04-29 14:59:00 +0200

    Added GuestTypeController

---
+++ Backend/src/main/kotlin/com/sorsix/backend/api/GuestTypeController.kt
@@ -0,0 +1,13 @@
+package com.sorsix.backend.api
+
+import com.sorsix.backend.repository.guest_type_repository.GuestTypeRepository
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/guest-type")
+class GuestTypeController(private val guestTypeRepository: GuestTypeRepository) {
+    @GetMapping
+    fun getAllGuestTypes() = guestTypeRepository.findAll()
+}
